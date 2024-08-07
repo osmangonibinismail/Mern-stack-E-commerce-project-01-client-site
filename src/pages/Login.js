c2974551c8b2b847f79dceb8fdac414a0ce9a873
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-07 22:23:57 +0600

    sign up page added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,66 @@
 import React, { useState } from 'react'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import loginIcons from '../assest/sigin.gif'
+import loginIcons from '../assest/sigin.gif';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value
+      }
+    })
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+  console.log('data login', data)
+
   return (
     <section id='login'>
       <div className='container mx-auto p-4'>
-        <div className='bg-white p-2 py-5 w-full max-w-md mx-auto '>
+        <div className='bg-white p-5 w-full max-w-sm mx-auto '>
           <div className='w-20 h-20 mx-auto'>
             <img src={loginIcons} alt='login icons' />
           </div>
           {/* <div className=''>
 
           </div> */}
-          <form className='' >
+          <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
             <div className='grid'>
               <label>Email : </label>
               <div className='bg-slate-100 p-2'>
-                <input type='email' placeholder='Enter email' className='w-full h-full outline-none bg-transparent'></input>
+                <input type='email'
+                 placeholder='Enter email'
+                 name='email'
+                 value={data.email}
+                 onChange={handleOnChange}
+                  className='w-full h-full outline-none bg-transparent'></input>
               </div>
             </div>
             <div className=''>
               <label>Password : </label>
               <div className='bg-slate-100 p-2 flex'>
-                <input type={showPassword ? "text" : "password"} placeholder='Enter password' className='w-full h-full outline-none bg-transparent'></input>
-                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((previous)=> ! previous)}>
+                <input
+                 type={showPassword ? "text" : "password"}
+                  placeholder='Enter password'
+                  name='password'
+                 value={data.password}
+                 onChange={handleOnChange}
+                 className='w-full h-full outline-none bg-transparent'
+                 ></input>
+                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((previous) => !previous)}>
                   <span>
                     {
                       showPassword ? (
@@ -40,10 +74,14 @@ const Login = () => {
                   </span>
                 </div>
               </div>
+              <Link to={'/forgot-password'} className="block w-fit ml-auto hover:underline hover:text-lime-500">
+                Forget password ?
+              </Link>
             </div>
 
-            <button className='bg-lime-500 text-white px-6 py-2 w-full max-w-[400px] rounded-lg hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+            <button className='bg-lime-500 hover:bg-lime-600 text-white px-6 py-2 w-full max-w-[400px] rounded-lg hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
           </form>
+          <p className='my-5'>Don't have account? please <Link to={"/sign-up"} className='text-lime-600 hover:text-lime-700 font-semibold hover:underline'>Sign up</Link></p>
 
         </div>
       </div>
---
+++ src/pages/SignUp.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import loginIcons from '../assest/sigin.gif';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        name: "",
+        confirmPassword: "",
+        profilePic: "",
+    })
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target
+
+        setData((preve) => {
+            return {
+                ...preve,
+                [name]: value
+            }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    console.log('data login', data)
+    return (
+        <section id='signup'>
+            <div className='container mx-auto p-4'>
+                <div className='bg-white p-5 w-full max-w-sm mx-auto '>
+                    <div className='w-20 h-20 mx-auto'>
+                        <img src={loginIcons} alt='login icons' />
+                    </div>
+                    <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+                        <div className='grid'>
+                            <label>Name : </label>
+                            <div className='bg-slate-100 p-2'>
+                                <input type='text'
+                                    placeholder='Enter your name'
+                                    name='name'
+                                    value={data.name}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'></input>
+                            </div>
+                        </div>
+                        <div className='grid'>
+                            <label>Email : </label>
+                            <div className='bg-slate-100 p-2'>
+                                <input type='email'
+                                    placeholder='Enter email'
+                                    name='email'
+                                    value={data.email}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'></input>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <label>Password : </label>
+                            <div className='bg-slate-100 p-2 flex'>
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder='Enter password'
+                                    name='password'
+                                    value={data.password}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'
+                                ></input>
+                                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((previous) => !previous)}>
+                                    <span>
+                                        {
+                                            showPassword ? (
+                                                <FaEyeSlash />
+                                            )
+                                                :
+                                                (
+                                                    <FaEye />
+                                                )
+                                        }
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <label>Confirm password : </label>
+                            <div className='bg-slate-100 p-2 flex'>
+                                <input
+                                    type={showConfirmPassword ? "text" : "password"}
+                                    placeholder='Enter confirm password'
+                                    name='confirmPassword'
+                                    value={data.confirmPassword}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'
+                                ></input>
+                                <div className='cursor-pointer text-xl' onClick={() => setShowConfirmPassword((previous) => !previous)}>
+                                    <span>
+                                        {
+                                            showConfirmPassword ? (
+                                                <FaEyeSlash />
+                                            )
+                                                :
+                                                (
+                                                    <FaEye />
+                                                )
+                                        }
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button className='bg-lime-500 hover:bg-lime-600 text-white px-6 py-2 w-full max-w-[400px] rounded-lg hover:scale-110 transition-all mx-auto block mt-6'>Sign up</button>
+                    </form>
+                    <p className='my-5'>Already have an account? please <Link to={"/login"} className='text-lime-600 hover:text-lime-700 font-semibold hover:underline'>Log in</Link></p>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SignUp;
