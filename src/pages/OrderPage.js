2affe9666b62aee3058d21fd60ba8416a3e0112b
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-06 12:16:46 +0600

    order page updated

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SummaryApi from '../common'
 
 const OrderPage = () => {
+  const [data, setData] = useState([])
+
+  const fetchOrderDetails = async () => {
+    const response = await fetch(SummaryApi.getOrder.url, {
+      method: SummaryApi.getOrder.method,
+      credentials: 'include'
+    })
+
+    const responseData = await response.json()
+    console.log("order list", responseData)
+  }
+
+  useEffect(() => {
+    fetchOrderDetails()
+  }, [])
   return (
     <div>
-      OrderPage
-      OrderPage
+      {
+        data[0] && (
+          <p>No Order Available</p>
+        )
+      }
+
+      <div>
+        {
+          data.map((item,index)=>{
+            return(
+              <div></div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
