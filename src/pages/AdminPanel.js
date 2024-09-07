bef8a91a8a5c4e6bf6e21d60a8763c23c3cb16cf
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-07 07:46:01 +0600

    admin panel and common index page updated

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -34,7 +34,8 @@ const AdminPanel = () => {
                 <div className=''>
                     <nav className='grid p-4'>
                         <Link to={"all-users"} className='px-2 py-1 hover:bg-slate-100'>All Users</Link>
-                        <Link to={"all-products"} className='px-2 py-1 hover:bg-slate-100'>All Product</Link>
+                        <Link to={"all-products"} className='px-2 py-1 hover:bg-slate-100'>All Products</Link>
+                        <Link to={"all-order"} className='px-2 py-1 hover:bg-slate-100'>All Orders</Link>
                     </nav>
                 </div>
             </aside>
