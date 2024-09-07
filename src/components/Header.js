f8de49af41b51c46eb7d951358c474f1854b3035
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-07 06:21:16 +0600

    order page and header page updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -100,6 +100,8 @@ const Header = () => {
                         <Link to={"/admin-panel/all-products"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={() => setMenuDisplay(preve => !preve)}>Admin Panel</Link>
                       )
                     }
+
+                    <Link to={'/order'} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={() => setMenuDisplay(preve => !preve)}>Order</Link>
                   </nav>
                 </div>
               )
--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import SummaryApi from '../common'
+import moment from 'moment'
+import displayINRCurrency from '../helpers/displayCurrency'
 
 const OrderPage = () => {
   const [data, setData] = useState([])
@@ -11,6 +13,8 @@ const OrderPage = () => {
     })
 
     const responseData = await response.json()
+
+    setData(responseData.data)
     console.log("order list", responseData)
   }
 
@@ -20,16 +24,67 @@ const OrderPage = () => {
   return (
     <div>
       {
-        data[0] && (
+        data.length === 0 && (
           <p>No Order Available</p>
         )
       }
 
-      <div>
+      <div className='p-4 w-full'>
         {
-          data.map((item,index)=>{
-            return(
-              <div></div>
+          data.map((item, index) => {
+            return (
+              <div key={item.userId + index}>
+                <p className='font-medium text-lg'> {moment(item.createdAt).format('LL')}</p>
+                <div className='border rounded'>
+                  <div className='flex flex-col lg:flex-row justify-between'>
+                    <div className='grid gap-1'>
+                      {
+                        item?.productDetails.map((product, index) => {
+                          return (
+                            <div key={product.productId + index} className='flex gap-3 bg-slate-100'>
+                              <img
+                                src={product.image[0]}
+                                className='w-28 h-28 bg-white object-scale-down'
+                              />
+                              <div>
+                                <div className='font-medium text-lg text-ellipsis line-clamp-1'>{product.name}</div>
+                                <div className='flex items-center gap-5 mt-1'>
+                                  <div className='text-lg text-lime-600'>{displayINRCurrency(product.price)}</div>
+                                  <p>Quantity : {product.quantity}</p>
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+
+                    <div className='flex flex-col gap-4 p-4 min-w-[320px]'>
+                      <div>
+                        <div className='text-lg font-medium'>Payment Details : </div>
+                        <p className='ml-1'>Payment method : {item.paymentDetails.payment_method_type[0]}</p>
+                        <p className='ml-1'>Payment status : {item.paymentDetails.payment_status}</p>
+                      </div>
+                      <div>
+                        <div className='text-lg font-medium'>Shipping Details : </div>
+                        {
+                          item.shipping_options.map((shipping, index) => {
+                            return (
+                              <div key={shipping.shipping_rate} className='ml-1'>
+                                Shipping Charge : {shipping.shipping_amount}
+                              </div>
+                            )
+                          })
+                        }
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='font-semibold w-fit ml-auto lg:text-lg'>
+                    Total Amount : ${item.totalAmount}
+                  </div>
+                </div>
+              </div>
             )
           })
         }
